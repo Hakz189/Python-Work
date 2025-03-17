9133cd5bf0bfba485157204026cb1ecd9f57603d
Divine <hakizacollege@gmail.com>
2025-03-17 21:02:59 +0400

    third version

--- python.py
+++ python.py
@@ -1,6 +1,15 @@
 # text = input("Enter a string: ")
 # print(f"The text is {len(text)} characters")
 
-text = input("Enter a string: ")
-text = text.replace(" ","")
-print(f"The text has {len(text)} characters")
+# text = input("Enter a string: ")
+# text = text.replace(" ","")
+# print(f"The text has {len(text)} characters")
+
+
+array = []
+z = 0
+while z < 5:
+    text = input("Enter a text: ").replace(" ", "")
+    array.append(f"{text} => {len(text)}")
+    z += 1
+print(array) 
